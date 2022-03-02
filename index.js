0f60ce3776adfbe8c8633adecf0ebee086df513c
amanda <ramosamanda2510@gmail.com>
2022-03-01 19:25:31 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const hqLocation = 42
+const blockInFeet = 264
+
+function distanceFromHqInBlocks(pickup){
+    if (pickup > hqLocation){
+        return pickup - hqLocation;
+    } else if (pickup < hqLocation){
+        return hqLocation - pickup;
+    }
+}
+
+function distanceFromHqInFeet(pickup){
+    if (pickup > hqLocation){
+        return ((pickup- hqLocation) * 264);
+    } else if (pickup < hqLocation){
+        return ((hqLocation - pickup) * 264);
+    }
+}
+
+function distanceTravelledInFeet(sDestination, fDestination){
+    let distance = Math.abs(sDestination - fDestination) * 264;
+    return distance;
+}    
+
+function calculatesFarePrice(sDestination, fDestination){
+    let blockDistance = Math.abs(fDestination - sDestination);
+    let distanceTraveled = blockDistance * 264; 
+    if (distanceTraveled <= 400){
+        console.log ('give customers a free sample');
+        return (0);
+    }else if (distanceTraveled > 400 && distanceTraveled <= 2000){
+        return ((distanceTraveled - 400) * .02); 
+    }else if (distanceTraveled > 2000 && distanceTraveled < 2500){
+        return (25);
+    }else if (distanceTraveled >= 2500){
+        return 'cannot travel that far';
+    }
+}
